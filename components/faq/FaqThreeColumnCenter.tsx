8c4b0c5908cb2c5aea29a64e57e4252e0e6d71fc
Poorva-WW <poorva@wisdomworks.co>
2023-02-27 11:33:50 +0530

    Add core components
    Refs #3

---
+++ components/faq/FaqThreeColumnCenter.tsx
@@ -0,0 +1,200 @@
+// JSON-DRIVEN FaqThreeColumnCenter Template - Simple Direct Editing
+'use client';
+
+import { type FC } from 'react';
+import metadata from '@/_metadata.json';
+import { useTheme } from '@/context/ThemeContext';
+import { getFontFamily } from '@/utils/themeUtils';
+import {
+  getContainerBackgroundColor,
+  getTitleColor,
+  getDescriptionColor,
+  getSecondaryTextColor,
+  getPrimaryButtonColor
+} from '@/utils/colorUtils';
+import { editWithAI } from '@/utils/simpleEditorWithAI';
+import { getEditableClasses, getEditableProps } from '@/utils/environment';
+
+interface Props {
+  pageSlug?: string;
+  componentId?: string;
+  theme?: any;
+  backgroundType?: 'primary' | 'secondary';
+  previewContent?: any;
+}
+
+// Helper function to get component content from metadata
+const getComponentContent = (pageSlug: string, componentId: string) => {
+  const page = metadata.pages?.find((p: any) => p.slug === pageSlug);
+  if (!page) return null;
+  
+  return page.components?.find((c: any) => 
+    c.id === componentId || c.type === 'faq-three-column-center'
+  );
+};
+
+export default function FaqThreeColumnCenter(props: Props) {
+  const {
+    pageSlug = 'home',
+    componentId = 'faq-three-column-center',
+    theme: propTheme,
+    backgroundType,
+    previewContent
+  } = props;
+  const { theme: contextTheme } = useTheme();
+
+  // Use theme from props (editor) or context
+  const currentTheme = propTheme || contextTheme;
+
+  // Get content from JSON metadata or use preview content
+  const componentData = !previewContent ? getComponentContent(pageSlug, componentId) : null;
+  const content = previewContent || componentData?.content || {};
+
+  if (!previewContent && (!content || Object.keys(content).length === 0)) {
+    console.error(`FaqThreeColumnCenter: content not found for ${pageSlug}/${componentId}`);
+    return null;
+  }
+
+  const {
+    headline = 'Frequently asked questions',
+    subheadline = 'Support',
+    description = "Have a different question and can't find the answer you're looking for? Reach out to our support team by sending us an email and we'll get back to you as soon as we can.",
+    faqs = []
+  } = content;
+
+  // Override backgroundType with prop if provided
+  const finalContent = {
+    ...content,
+    backgroundType: backgroundType || content.backgroundType || 'primary'
+  };
+
+  // Theme utilities using standardized color functions
+  const containerBackground = getContainerBackgroundColor({ theme: currentTheme, content: finalContent });
+  const headingColor = getTitleColor({ theme: currentTheme }); // For main title and questions
+  const bodyTextColor = getDescriptionColor({ theme: currentTheme }); // For main description and answers
+  const metaTextColor = getSecondaryTextColor({ theme: currentTheme }); // For subtitle
+  const linkColor = getPrimaryButtonColor({ theme: currentTheme }); // For links in description
+  const headingFont = getFontFamily(currentTheme, 'heading');
+  const bodyFont = getFontFamily(currentTheme, 'body');
+
+  return (
+    <div 
+      className="py-16 sm:py-24 transition-colors duration-200"
+      style={{ backgroundColor: containerBackground }}
+    >
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          {subheadline && (
+            <h2
+              className={`text-base font-semibold leading-7 transition-colors duration-200 ${getEditableClasses()}`}
+              style={{
+                color: metaTextColor,
+                fontFamily: headingFont
+              }}
+              {...getEditableProps(
+                (e) => {
+                  e.stopPropagation();
+                  editWithAI(pageSlug, componentId, 'subheadline', subheadline, 'FAQ Subheadline');
+                },
+                "Click to edit FAQ subheadline"
+              )}
+            >
+              {subheadline}
+            </h2>
+          )}
+
+          <h2
+            className={`text-4xl font-semibold tracking-tight sm:text-5xl transition-colors duration-200 ${getEditableClasses()} ${subheadline ? 'mt-2' : ''}`}
+            style={{
+              color: headingColor,
+              fontFamily: headingFont
+            }}
+            {...getEditableProps(
+              (e) => {
+                e.stopPropagation();
+                editWithAI(pageSlug, componentId, 'headline', headline, 'FAQ Headline');
+              },
+              "Click to edit FAQ headline"
+            )}
+          >
+            {headline}
+          </h2>
+
+          {description && (
+            <p
+              className={`mt-6 text-base/7 transition-colors duration-200 ${getEditableClasses()}`}
+              style={{
+                color: bodyTextColor,
+                fontFamily: bodyFont
+              }}
+              {...getEditableProps(
+                (e) => {
+                  e.stopPropagation();
+                  editWithAI(pageSlug, componentId, 'description', description, 'FAQ Description');
+                },
+                "Click to edit FAQ description"
+              )}
+            >
+              {description.includes('sending us an email') ? (
+                <>
+                  {description.split('sending us an email')[0]}
+                  <a 
+                    href="#" 
+                    className="font-semibold hover:opacity-80 transition-opacity duration-200"
+                    style={{ color: linkColor }}
+                  >
+                    sending us an email
+                  </a>
+                  {description.split('sending us an email')[1]}
+                </>
+              ) : (
+                description
+              )}
+            </p>
+          )}
+        </div>
+        
+        <div className="mt-20">
+          <dl className="space-y-16 sm:grid sm:grid-cols-2 sm:space-y-0 sm:gap-x-6 sm:gap-y-16 lg:grid-cols-3 lg:gap-x-10">
+            {faqs.map((faq: any, index: number) => (
+              <div key={faq.question || index}>
+                <dt
+                  className={`text-base/7 font-semibold transition-colors duration-200 ${getEditableClasses()}`}
+                  style={{
+                    color: headingColor,
+                    fontFamily: headingFont
+                  }}
+                  {...getEditableProps(
+                    (e) => {
+                      e.stopPropagation();
+                      editWithAI(pageSlug, componentId, `faqs.${index}.question`, faq.question, `FAQ ${index + 1} Question`);
+                    },
+                    `Click to edit FAQ ${index + 1} question`
+                  )}
+                >
+                  {faq.question}
+                </dt>
+                <dd
+                  className={`mt-2 text-base/7 transition-colors duration-200 ${getEditableClasses()}`}
+                  style={{
+                    color: bodyTextColor,
+                    fontFamily: bodyFont
+                  }}
+                  {...getEditableProps(
+                    (e) => {
+                      e.stopPropagation();
+                      editWithAI(pageSlug, componentId, `faqs.${index}.answer`, faq.answer, `FAQ ${index + 1} Answer`);
+                    },
+                    `Click to edit FAQ ${index + 1} answer`
+                  )}
+                >
+                  {faq.answer}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/hero/HeroImageTop.tsx
@@ -0,0 +1,261 @@
+// JSON-DRIVEN HeroImageTop Template
+'use client';
+
+import React, { useState } from 'react';
+import metadata from '@/_metadata.json';
+import { useTheme } from '@/context/ThemeContext';
+import {
+  getFontFamily,
+  normalizeImageData,
+  normalizeCTAData
+} from '@/utils/themeUtils';
+import {
+  getContainerBackgroundColor,
+  getTitleColor,
+  getDescriptionColor,
+  getPrimaryButtonColor,
+  getPrimaryButtonTextColor,
+  getSecondaryTextColor,
+  getFeatureCardBackgroundColor
+} from '@/utils/colorUtils';
+import { editWithAI } from '@/utils/simpleEditorWithAI';
+import { getEditableClasses, getEditableProps } from '@/utils/environment';
+
+interface Props {
+  pageSlug?: string;
+  componentId?: string;
+  theme?: any;
+  backgroundType?: 'primary' | 'secondary';
+  previewContent?: any;
+}
+
+// Helper function to get component content from metadata
+const getComponentContent = (pageSlug: string, componentId: string) => {
+  const page = metadata.pages?.find((p: any) => p.slug === pageSlug);
+  if (!page) return null;
+  
+  return page.components?.find((c: any) => c.id === componentId || c.type === 'hero-image-top');
+};
+
+export default function HeroImageTop(props: Props) {
+  const {
+    pageSlug = 'home',
+    componentId = 'hero-image-top',
+    theme: propTheme,
+    backgroundType,
+    previewContent
+  } = props;
+  const { theme: contextTheme } = useTheme();
+
+  // Use theme from props (editor) or context
+  const currentTheme = propTheme || contextTheme;
+
+  const [imageError, setImageError] = useState(false);
+
+  // Use preview content if provided, otherwise get from metadata
+  let content;
+  if (previewContent) {
+    content = previewContent;
+  } else {
+    const componentData = getComponentContent(pageSlug, componentId);
+    content = componentData?.content || {};
+  }
+  
+  if (!content || Object.keys(content).length === 0) {
+    console.error(`HeroImageTop: content not found for ${pageSlug}/${componentId}`);
+    return null;
+  }
+
+
+  // Extract content properties
+  const {
+    headline = 'Your Headline Here',
+    description = 'Your compelling description text goes here to explain your product or service in detail',
+    cta_primary = { text: 'Explore More', href: '#' },
+    cta_secondary = null,
+    announcement = 'New Feature',
+    image = { src: 'https://storage.googleapis.com/site-media-asia-01/common/placeholder-image.png', alt: 'Hero showcase' },
+    stats = [
+      { value: '15+', label: 'Years Experience' },
+      { value: '500+', label: 'Projects Completed' },
+      { value: '98%', label: 'Client Satisfaction' },
+      { value: '24/7', label: 'Support Available' }
+    ],
+  } = content;
+
+  // Override backgroundType with prop if provided
+  const finalContent = {
+    ...content,
+    backgroundType: backgroundType || content.backgroundType || 'primary'
+  };
+
+  // Color and style helpers using standardized color utilities
+  const containerBackgroundColor = getContainerBackgroundColor({ theme: currentTheme, content: finalContent });
+  const headlineColor = getTitleColor({ theme: currentTheme });
+  const descriptionColor = getDescriptionColor({ theme: currentTheme });
+  const primaryButtonBackground = getPrimaryButtonColor({ theme: currentTheme });
+  const primaryButtonText = getPrimaryButtonTextColor({ theme: currentTheme });
+  const announcementBackground = getFeatureCardBackgroundColor({ theme: currentTheme });
+  const announcementColor = getSecondaryTextColor({ theme: currentTheme });
+  const statsValueColor = getTitleColor({ theme: currentTheme });
+  const statsLabelColor = getSecondaryTextColor({ theme: currentTheme });
+
+  const primaryCTA = normalizeCTAData(cta_primary);
+  const secondaryCTA = normalizeCTAData(cta_secondary);
+
+  const getAnnouncementStyle = () => ({
+    backgroundColor: announcementBackground,
+    color: announcementColor,
+    fontFamily: getFontFamily(currentTheme, 'body'),
+  });
+
+  const imageData = normalizeImageData(image, 'hero-image-top');
+  const imageSrc = imageData.src;
+
+  return (
+    <div 
+      className="min-h-screen transition-colors duration-200"
+      style={{ backgroundColor: containerBackgroundColor }}
+    >
+      {/* Hero Section */}
+      <section className="w-full pt-10 pb-10">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="w-full flex-col justify-start items-start xl:gap-20 lg:gap-14 gap-12 inline-flex">
+            {/* Hero Image */}
+            <div className="relative w-full group">
+
+              {!imageError ? (
+                <img
+                  className="lg:h-auto md:h-[460px] h-[400px] w-full rounded-3xl object-cover transition-opacity duration-200"
+                  src={imageSrc}
+                  alt={imageData.alt}
+                  onError={() => setImageError(true)}
+                />
+              ) : (
+                <div
+                  className={`lg:h-auto md:h-[460px] h-[400px] w-full rounded-3xl bg-gray-100 flex items-center justify-center ${getEditableClasses()}`}
+                  {...getEditableProps((e) => {
+                    e.stopPropagation();
+                    editWithAI(pageSlug, componentId, 'image.src', '', 'Image', 'image');
+                  }, "Click to add image")}
+                >
+                  <span className="text-gray-500">Click to add image</span>
+                </div>
+              )}
+            </div>
+
+            {/* Content Grid */}
+            <div className="w-full justify-start lg:items-start items-center xl:gap-10 gap-8 grid lg:grid-cols-2 grid-cols-1">
+              {/* Left Column - Heading */}
+              <div className="w-full flex-col justify-start lg:items-start items-center gap-5 inline-flex">
+                {announcement && (
+                  <div
+                    className={`px-2.5 py-0.5 rounded-full justify-start items-center gap-1.5 flex transition-colors duration-200 ${getEditableClasses()}`}
+                    style={getAnnouncementStyle()}
+                    {...getEditableProps((e) => {
+                      e.stopPropagation();
+                      editWithAI(pageSlug, componentId, 'announcement.text', announcement || '', 'Announcement');
+                    }, "Click to edit announcement")}
+                  >
+                    <span className="text-center text-xs font-medium leading-normal">
+                      {announcement}
+                    </span>
+                  </div>
+                )}
+                <h1
+                  className={`text-5xl font-bold font-sans leading-snug lg:text-start text-center transition-colors duration-200 ${getEditableClasses()}`}
+                  style={{
+                    color: headlineColor,
+                    fontFamily: getFontFamily(currentTheme, 'heading'),
+                  }}
+                  {...getEditableProps((e) => {
+                    e.stopPropagation();
+                    editWithAI(pageSlug, componentId, 'headline', headline, 'Headline');
+                  }, "Click to edit headline")}
+                >
+                  {headline}
+                </h1>
+              </div>
+
+              {/* Right Column - Description & CTA */}
+              <div className="w-full h-full flex-col justify-between lg:items-start items-center inline-flex lg:gap-0 gap-5">
+                <p
+                  className={`text-base font-normal leading-relaxed lg:text-start text-center transition-colors duration-200 ${getEditableClasses()}`}
+                  {...getEditableProps((e) => {
+                    e.stopPropagation();
+                    editWithAI(pageSlug, componentId, 'description', description, 'Description');
+                  }, "Click to edit description")}
+                  style={{
+                    color: descriptionColor,
+                    fontFamily: getFontFamily(currentTheme, 'body'),
+                  }}
+                >
+                  {description}
+                </p>
+                {cta_primary && (
+                  <div className="rounded-md shadow">
+                    <button
+                      className={`sm:w-fit w-full px-5 py-2.5 hover:opacity-90 transition-all duration-700 ease-in-out rounded-full shadow-sm justify-center items-center flex ${getEditableClasses()}`}
+                      style={{
+                        backgroundColor: primaryButtonBackground,
+                        color: primaryButtonText,
+                        fontFamily: getFontFamily(currentTheme, 'body'),
+                      }}
+                      {...getEditableProps((e) => {
+                        e.preventDefault();
+                        editWithAI(pageSlug, componentId, 'cta_primary.text', primaryCTA.text, 'Primary Button Text');
+                      }, "Click to edit button text")}
+                    >
+                      <span className="px-2 py-px text-base font-semibold leading-relaxed">
+                        {primaryCTA.text}
+                      </span>
+                      <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                      </svg>
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            {/* Stats Section */}
+            {stats && stats.length > 0 && (
+              <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-8">
+                {stats.map((stat: any, index: number) => (
+                  <div key={index} className="text-center">
+                    <div
+                      className={`text-3xl font-bold transition-colors duration-200 ${getEditableClasses()}`}
+                      style={{
+                        color: statsValueColor,
+                        fontFamily: getFontFamily(currentTheme, 'heading'),
+                      }}
+                      {...getEditableProps((e) => {
+                        e.stopPropagation();
+                        editWithAI(pageSlug, componentId, `stats.${index}.value`, stat.value, `Stat ${index + 1} Value`);
+                      }, "Click to edit stat value")}
+                    >
+                      {stat.value}
+                    </div>
+                    <div
+                      className={`text-sm mt-1 transition-colors duration-200 ${getEditableClasses()}`}
+                      style={{
+                        color: statsLabelColor,
+                        fontFamily: getFontFamily(currentTheme, 'body'),
+                      }}
+                      {...getEditableProps((e) => {
+                        e.stopPropagation();
+                        editWithAI(pageSlug, componentId, `stats.${index}.label`, stat.label, `Stat ${index + 1} Label`);
+                      }, "Click to edit stat label")}
+                    >
+                      {stat.label}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
