4a4565b8f1b586de50e9d61f8023dcadb4ef8efd
Poorva-WW <poorva@wisdomworks.co>
2023-06-25 18:25:19 +0530

    Implement dropdown menus
    Refs #11

---
+++ views/PatientCareView.tsx
@@ -0,0 +1,138 @@
+'use client';
+
+/**
+ * PatientCare View
+ * Generated with inlined content - clean standalone code
+ */
+
+export default function PatientCareView() {
+  return (
+    <main className="min-h-screen bg-white">
+      {/* Hero Section */}
+      <section className="bg-white py-16 lg:py-24">
+        <div className="max-w-7xl mx-auto px-6 lg:grid lg:grid-cols-2 lg:gap-16 items-center">
+          <div>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 mb-6">Compassionate Care, Modern Medicine, Historic Excellence</h1>
+            <p className="text-xl text-gray-600 mb-8">Over 157 years of trusted healthcare service. From emergency care to specialized treatments, we provide comprehensive medical services to all with dignity and excellence.</p>
+            
+            <div className="flex flex-wrap gap-4">
+              
+              
+            </div>
+          </div>
+          <div className="mt-12 lg:mt-0">
+            <img src="/placeholder.jpg" alt="Hero image" className="rounded-xl shadow-2xl w-full" />
+          </div>
+        </div>
+      </section>
+      {/* Features Section */}
+      <section className="bg-gray-50 py-16 lg:py-24">
+        <div className="max-w-7xl mx-auto px-6">
+          <div className="text-center mb-16">
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">Specialized Centers of Excellence</h2>
+            <p className="text-xl text-gray-600 max-w-3xl mx-auto">Advanced medical services and specialized care facilities</p>
+          </div>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">ART Centre - HIV/AIDS Care</h3>
+                <p className="text-gray-600">Nodal Centre recognized by National AIDS Control Organisation providing comprehensive Anti-Retroviral Therapy and support services.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">Blood Bank (Raktha Kosha)</h3>
+                <p className="text-gray-600">24-hour blood bank services with ID NAT facility ensuring safe blood transfusions with advanced screening technology.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">Joint Replacement Centre</h3>
+                <p className="text-gray-600">State-of-the-art joint replacement surgeries with modular operation theatre and comprehensive orthopedic care.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">Artificial Limb Centre</h3>
+                <p className="text-gray-600">Prosthetic and orthotic services providing artificial limbs and rehabilitation support for mobility restoration.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">Dialysis Services</h3>
+                <p className="text-gray-600">Modern dialysis facilities for patients with kidney disorders, providing life-saving renal replacement therapy.</p>
+              </div>
+              <div className="bg-white p-6 rounded-xl shadow-sm">
+                <h3 className="text-lg font-semibold text-gray-900 mb-2">Nutrition Rehabilitation Centre</h3>
+                <p className="text-gray-600">Specialized care for Severe Acute Malnutrition (SAM) with comprehensive nutritional support and monitoring.</p>
+              </div>
+          </div>
+        </div>
+      </section>
+      {/* Content Section */}
+      <section className="bg-white py-16 lg:py-24">
+        <div className="max-w-7xl mx-auto px-6 lg:grid lg:grid-cols-2 lg:gap-16 items-center">
+          <div>
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4">Your Journey to Quality Healthcare</h2>
+            <p className="text-xl text-indigo-600 mb-6">Simple Steps to Access Care</p>
+            <div className="text-gray-600 leading-relaxed whitespace-pre-line"></div>
+          </div>
+          <div className="mt-12 lg:mt-0">
+            <img src="https://picsum.photos/800/600?random=5&grayscale" alt="hospital patient registration consultation India healthcare journey" className="rounded-xl w-full" />
+          </div>
+        </div>
+      </section>
+      {/* FAQ Section */}
+      <section className="bg-gray-50 py-16 lg:py-24">
+        <div className="max-w-3xl mx-auto px-6">
+          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 text-center mb-12">Frequently Asked Questions About Patient Care</h2>
+          <div className="space-y-4">
+            
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">What are the OPD timings and visiting hours?</summary>
+                <p className="mt-4 text-gray-600">OPD services operate from 9:00 AM to 5:00 PM on weekdays. Patient visiting hours are typically 10:00 AM - 12:00 PM and 4:00 PM - 6:00 PM. Emergency services are available 24/7 throughout the year.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">What government health schemes are accepted?</summary>
+                <p className="mt-4 text-gray-600">We implement Ayushman Bharat Arogya Karnataka (ABARK), free care for SC/ST patients, 50% discount for BPL cardholders, Jyothi Sanjeevani Scheme for government officials, Yeshaswini Scheme, Janani Shishu Suraksha Karyakrama, and Chief Minister Medical Relief Fund.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">Do I need to bring any documents for admission?</summary>
+                <p className="mt-4 text-gray-600">Yes, please bring government-issued photo ID (Aadhaar card preferred), any previous medical records, prescriptions, diagnostic reports, and relevant government scheme cards (Ayushman Bharat, BPL, SC/ST certificates) if applicable.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">What emergency services are available?</summary>
+                <p className="mt-4 text-gray-600">Our 24/7 Emergency Department provides immediate care for all medical emergencies with 2 dedicated emergency operation theatres, trauma care, critical care beds, ICU with ventilators, and ambulance services.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">Are diagnostic services available on all days?</summary>
+                <p className="mt-4 text-gray-600">Yes, our diagnostic services including X-ray, CT scan, pathology lab, and biochemistry are available daily. Emergency diagnostic services operate 24/7. Routine tests are available during regular OPD hours.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">What maternity services do you provide?</summary>
+                <p className="mt-4 text-gray-600">We conduct 420-450 deliveries monthly with comprehensive obstetric care, NICU, SNCU (Sick Newborn Care Unit), Kangaroo Mother Care, and free treatment for children up to 1 year under Janani Shishu Suraksha Karyakrama.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">Is there a 24-hour pharmacy and blood bank?</summary>
+                <p className="mt-4 text-gray-600">Yes, both our pharmacy and blood bank (Raktha Kosha) operate 24 hours daily. The blood bank is equipped with ID NAT facility for advanced blood screening and safety.</p>
+              </details>
+              <details className="bg-white p-6 rounded-lg shadow-sm">
+                <summary className="font-semibold text-gray-900 cursor-pointer">How many beds are available for admission?</summary>
+                <p className="mt-4 text-gray-600">We have 686 beds across various departments. On average, 70-80 patients are admitted daily. For non-emergency admissions, please consult with the concerned department for bed availability.</p>
+              </details>
+          </div>
+        </div>
+      </section>
+      {/* Contact Section */}
+      <section className="bg-white py-16 lg:py-24">
+        <div className="max-w-7xl mx-auto px-6 lg:grid lg:grid-cols-2 lg:gap-16">
+          <div>
+            <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-8">Need Assistance? We're Here to Help</h2>
+            <div className="space-y-4 text-gray-600">
+              
+              
+              
+            </div>
+          </div>
+          <div className="mt-12 lg:mt-0">
+            <div className="bg-gray-100 rounded-xl h-64 flex items-center justify-center text-gray-500">
+              Map / Contact Form
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
